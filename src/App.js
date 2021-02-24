e1ff177bc5c90dc9744ba9839352e35d3b9693e7
Keane Gleeson <keanegleeson@gmail.com>
2021-02-23 19:34:33 -0500

    2/23 session

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as ReactBootStrap from "react-bootstrap";
 
-function App() {
+
+
+const App = () => {
+  const projects = [
+    {project: "a", technology:"Wind", usage:"100"},
+    {project: "b", technology:"Solar", usage:"50"},
+    {project: "c", technology:"Wind", usage:"40"},
+    {project: "d", technology:"Solar", usage:"25"},
+    {project: "e", technology:"Geothermal", usage:"5"},
+  ]
+
+  const renderProject = (project, index) => {
+    return(
+      <tr key={index}>
+        <td>{project.project} </td>
+        <td>{project.technology} </td>
+        <td>{project.usage} </td>
+      </tr>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactBootStrap.Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Project Name</th>
+      <th>Technology</th>
+      <th>Usage</th>
+    </tr>
+  </thead>
+  <tbody>
+    {projects.map(renderProject)}
+  </tbody>
+</ReactBootStrap.Table>
     </div>
-  );
+  )
 }
+
 
 export default App;
